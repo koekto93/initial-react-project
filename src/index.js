6b5fd85f545345dd68afc18fe60e8d3a48de1b5a
Mark <koekto93@mail.ru>
2018-10-04 16:57:52 +0300

    second initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router'
+import history from './history'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './components/App'
+
+import './index.css'
+
+import createStore from './redux/create'
+
+const store = createStore()
+
+window.store = store
+
+ReactDOM.render(
+  <Router history={history}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById('root')
+)
